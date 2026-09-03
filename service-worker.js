3198ca46ddfba42dfc5492b1713960423714fc96
outofmycmftzone-ux <outofmycmftzone@gmail.com>
2026-09-03 13:31:16 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,64 @@
-const CACHE_NAME = "spicy-couples-bingo-v5";
+const CACHE_NAME = "spicy-couples-bingo-v6";
 
 const FILES = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./prompts.json",
-    "./manifest.json"
+"./",
+"./index.html",
+"./style.css",
+"./script.js",
+"./prompts.json",
+"./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES))
-            .then(() => self.skipWaiting())
-    );
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(FILES))
+.then(() => self.skipWaiting())
+);
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys()
-            .then(keys =>
-                Promise.all(
-                    keys
-                        .filter(key => key !== CACHE_NAME)
-                        .map(key => caches.delete(key))
-                )
-            )
-            .then(() => self.clients.claim())
-    );
+event.waitUntil(
+caches.keys()
+.then(keys =>
+Promise.all(
+keys
+.filter(key => key !== CACHE_NAME)
+.map(key => caches.delete(key))
+)
+)
+.then(() => self.clients.claim())
+);
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        fetch(event.request)
-            .then(response => {
-                const copy = response.clone();
+if (new URL(event.request.url).pathname.endsWith("/prompts.json")) {
+event.respondWith(
+fetch(event.request, {
+cache: "no-store"
+})
+.then(response => {
+return response;
+})
+.catch(() => caches.match(event.request))
+);
+return;
+}
 
-                caches.open(CACHE_NAME).then(cache => {
-                    cache.put(event.request, copy);
-                });
+event.respondWith(
+    fetch(event.request)
+        .then(response => {
+            const copy = response.clone();
 
-                return response;
-            })
-            .catch(() =>
-                caches.match(event.request)
-            )
-    );
+            caches.open(CACHE_NAME).then(cache => {
+                cache.put(event.request, copy);
+            });
+
+            return response;
+        })
+        .catch(() =>
+            caches.match(event.request)
+        )
+);
+
 });
